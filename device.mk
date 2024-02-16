7f176b13d3ac127ad51c22df9a0c6d7572dd0a2b
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2024-02-15 21:45:01 -0300

    fog: Build capabilityconfigstore@1.0 vendor variant
    * F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.capabilityconfigstore@1.0-service": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by main executable
    
    Change-Id: I7e10f40f1e1c357d65b71386ef87d8d777c6518b
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- device.mk
+++ device.mk
@@ -130,6 +130,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
+
 # Dex/ART optimization
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
