726790be92893dd66767eee871483a744e4ea4f0
alternoegraha <noegrahachan@gmail.com>
2024-11-17 18:30:37 +0700

    fog: vendorsetup: Switch vendor branch
    - now vendor branch are separated from fourteen due to removal of OMX libraries for A14 QPR3
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone -b fourteen https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone -b pixelbuilds https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
 git clone --depth=1 -b fog-r-oss-staging https://github.com/alternoegraha/fog_kernel_xiaomi_sm6225 kernel/xiaomi/fog
