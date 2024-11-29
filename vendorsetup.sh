37216516545a693a04b4b020b4fe3e34b294bdc6
alternoegraha <noegrahachan@gmail.com>
2024-11-29 07:11:58 +0000

    fog: vendorsetup: switch vendor branch
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone -b lineage-21-staging https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
 git clone --depth=1 -b fog-r-oss-staging https://github.com/alternoegraha/fog_kernel_xiaomi_sm6225 kernel/xiaomi/fog
