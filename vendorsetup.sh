67230569d9a97c7acb6bc4687cc57b9e7d952341
alternoegraha <37102951+alternoegraha@users.noreply.github.com>
2024-12-18 00:44:43 +0000

    fog: vendorsetup: update
    Signed-off-by: alternoegraha <37102951+alternoegraha@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,13 +2,13 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone -b lineage-22 https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone -b pixelage https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
 git clone --depth=1 -b fog-r-oss-staging https://github.com/alternoegraha/fog_kernel_xiaomi_sm6225 kernel/xiaomi/fog
 
 # Clone LineageOS' hardware/xiaomi
-git clone -b lineage-22.0 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone -b 15 https://github.com/ProjectPixelage/android_hardware_xiaomi hardware/xiaomi
 
 # Kernel fixups
 cd kernel/xiaomi/fog
